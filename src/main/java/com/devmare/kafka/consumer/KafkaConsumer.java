5ba6b1941eb78b6c81167fe23ca8f67d16d49324
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2025-03-17 21:12:55 +0530

    Initial commit

---
+++ src/main/java/com/devmare/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.devmare.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(
+            topics = "devmare",
+            groupId = "devGroup"
+    )
+    public void consumeMessage(String message) {
+        log.info("Cosuming the message from devmare topic: {}", message);
+    }
+}
